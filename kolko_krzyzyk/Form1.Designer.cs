8b419037e6a5bd01f002c535ba00aa01cdd7c709
eryk1995 <chiracle@gmail.com>
2016-12-06 18:13:01 +0100

    menu Nowa gra(dla wygody)

--- kolko_krzyzyk/Form1.Designer.cs
+++ kolko_krzyzyk/Form1.Designer.cs
@@ -55,6 +55,7 @@
             this.gracz1_licznik = new System.Windows.Forms.Label();
             this.remis_licznik = new System.Windows.Forms.Label();
             this.gracz2_licznik = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.groupBox1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.gracz2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.gracz1)).BeginInit();
@@ -357,12 +358,24 @@
             this.gracz2_licznik.TabIndex = 24;
             this.gracz2_licznik.Text = "Gracz 2";
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(435, 80);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.button1.Size = new System.Drawing.Size(119, 49);
+            this.button1.TabIndex = 30;
+            this.button1.Text = "Nowa gra";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveCaption;
             this.ClientSize = new System.Drawing.Size(561, 559);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.gracz2_licz);
             this.Controls.Add(this.remis_licz);
             this.Controls.Add(this.gracz1_licz);
@@ -432,6 +445,7 @@
         private System.Windows.Forms.Label gracz1_licznik;
         private System.Windows.Forms.Label remis_licznik;
         private System.Windows.Forms.Label gracz2_licznik;
+        private System.Windows.Forms.Button button1;
     }
 }
 
