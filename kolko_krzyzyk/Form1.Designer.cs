142e226b3f7399b2e25aec699ed61deb50b99ab5
eryk1995 <chiracle@gmail.com>
2016-12-06 17:22:02 +0100

    dodawnie menu

--- kolko_krzyzyk/Form1.Designer.cs
+++ kolko_krzyzyk/Form1.Designer.cs
@@ -28,19 +28,246 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.box8 = new System.Windows.Forms.PictureBox();
+            this.box7 = new System.Windows.Forms.PictureBox();
+            this.box6 = new System.Windows.Forms.PictureBox();
+            this.box5 = new System.Windows.Forms.PictureBox();
+            this.box4 = new System.Windows.Forms.PictureBox();
+            this.box3 = new System.Windows.Forms.PictureBox();
+            this.box2 = new System.Windows.Forms.PictureBox();
+            this.box1 = new System.Windows.Forms.PictureBox();
+            this.box0 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblgracz2 = new System.Windows.Forms.Label();
+            this.lblgracz1 = new System.Windows.Forms.Label();
+            this.gracz2 = new System.Windows.Forms.PictureBox();
+            this.gracz1 = new System.Windows.Forms.PictureBox();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.box8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box0)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gracz2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gracz1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.box8);
+            this.groupBox1.Controls.Add(this.box7);
+            this.groupBox1.Controls.Add(this.box6);
+            this.groupBox1.Controls.Add(this.box5);
+            this.groupBox1.Controls.Add(this.box4);
+            this.groupBox1.Controls.Add(this.box3);
+            this.groupBox1.Controls.Add(this.box2);
+            this.groupBox1.Controls.Add(this.box1);
+            this.groupBox1.Controls.Add(this.box0);
+            this.groupBox1.Location = new System.Drawing.Point(139, 80);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(284, 297);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // box8
+            // 
+            this.box8.BackColor = System.Drawing.Color.White;
+            this.box8.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.box8.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.box8.Location = new System.Drawing.Point(194, 201);
+            this.box8.Name = "box8";
+            this.box8.Size = new System.Drawing.Size(72, 72);
+            this.box8.TabIndex = 18;
+            this.box8.TabStop = false;
+            // 
+            // box7
+            // 
+            this.box7.BackColor = System.Drawing.Color.White;
+            this.box7.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.box7.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.box7.Location = new System.Drawing.Point(107, 201);
+            this.box7.Name = "box7";
+            this.box7.Size = new System.Drawing.Size(72, 72);
+            this.box7.TabIndex = 17;
+            this.box7.TabStop = false;
+            // 
+            // box6
+            // 
+            this.box6.BackColor = System.Drawing.Color.White;
+            this.box6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.box6.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.box6.Location = new System.Drawing.Point(19, 201);
+            this.box6.Name = "box6";
+            this.box6.Size = new System.Drawing.Size(72, 72);
+            this.box6.TabIndex = 16;
+            this.box6.TabStop = false;
+            // 
+            // box5
+            // 
+            this.box5.BackColor = System.Drawing.Color.White;
+            this.box5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.box5.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.box5.Location = new System.Drawing.Point(194, 114);
+            this.box5.Name = "box5";
+            this.box5.Size = new System.Drawing.Size(72, 72);
+            this.box5.TabIndex = 15;
+            this.box5.TabStop = false;
+            // 
+            // box4
+            // 
+            this.box4.BackColor = System.Drawing.Color.White;
+            this.box4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.box4.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.box4.Location = new System.Drawing.Point(107, 114);
+            this.box4.Name = "box4";
+            this.box4.Size = new System.Drawing.Size(72, 72);
+            this.box4.TabIndex = 14;
+            this.box4.TabStop = false;
+            // 
+            // box3
+            // 
+            this.box3.BackColor = System.Drawing.Color.White;
+            this.box3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.box3.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.box3.Location = new System.Drawing.Point(19, 114);
+            this.box3.Name = "box3";
+            this.box3.Size = new System.Drawing.Size(72, 72);
+            this.box3.TabIndex = 13;
+            this.box3.TabStop = false;
+            // 
+            // box2
+            // 
+            this.box2.BackColor = System.Drawing.Color.White;
+            this.box2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.box2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.box2.Location = new System.Drawing.Point(194, 24);
+            this.box2.Name = "box2";
+            this.box2.Size = new System.Drawing.Size(72, 72);
+            this.box2.TabIndex = 12;
+            this.box2.TabStop = false;
+            // 
+            // box1
+            // 
+            this.box1.BackColor = System.Drawing.Color.White;
+            this.box1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.box1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.box1.Location = new System.Drawing.Point(107, 24);
+            this.box1.Name = "box1";
+            this.box1.Size = new System.Drawing.Size(72, 72);
+            this.box1.TabIndex = 11;
+            this.box1.TabStop = false;
+            // 
+            // box0
+            // 
+            this.box0.BackColor = System.Drawing.Color.White;
+            this.box0.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.box0.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.box0.Location = new System.Drawing.Point(19, 24);
+            this.box0.Name = "box0";
+            this.box0.Size = new System.Drawing.Size(72, 72);
+            this.box0.TabIndex = 10;
+            this.box0.TabStop = false;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Tahoma", 27.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.label3.Location = new System.Drawing.Point(12, 19);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(551, 58);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Kółko i krzyżyk";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblgracz2
+            // 
+            this.lblgracz2.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblgracz2.ForeColor = System.Drawing.Color.Blue;
+            this.lblgracz2.Location = new System.Drawing.Point(429, 296);
+            this.lblgracz2.Name = "lblgracz2";
+            this.lblgracz2.Size = new System.Drawing.Size(131, 32);
+            this.lblgracz2.TabIndex = 15;
+            this.lblgracz2.Text = "Gracz 2";
+            // 
+            // lblgracz1
+            // 
+            this.lblgracz1.Font = new System.Drawing.Font("Verdana", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblgracz1.ForeColor = System.Drawing.Color.Red;
+            this.lblgracz1.Location = new System.Drawing.Point(2, 296);
+            this.lblgracz1.Name = "lblgracz1";
+            this.lblgracz1.Size = new System.Drawing.Size(131, 32);
+            this.lblgracz1.TabIndex = 14;
+            this.lblgracz1.Text = "Gracz 1";
+            // 
+            // gracz2
+            // 
+            this.gracz2.Location = new System.Drawing.Point(465, 341);
+            this.gracz2.Name = "gracz2";
+            this.gracz2.Size = new System.Drawing.Size(66, 72);
+            this.gracz2.TabIndex = 18;
+            this.gracz2.TabStop = false;
+            // 
+            // gracz1
+            // 
+            this.gracz1.Location = new System.Drawing.Point(20, 341);
+            this.gracz1.Name = "gracz1";
+            this.gracz1.Size = new System.Drawing.Size(72, 72);
+            this.gracz1.TabIndex = 17;
+            this.gracz1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(435, 416);
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(561, 559);
+            this.Controls.Add(this.gracz2);
+            this.Controls.Add(this.gracz1);
+            this.Controls.Add(this.lblgracz2);
+            this.Controls.Add(this.lblgracz1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.groupBox1);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kółko i krzyżyk";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.box8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.box0)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gracz2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gracz1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.PictureBox box8;
+        private System.Windows.Forms.PictureBox box7;
+        private System.Windows.Forms.PictureBox box6;
+        private System.Windows.Forms.PictureBox box5;
+        private System.Windows.Forms.PictureBox box4;
+        private System.Windows.Forms.PictureBox box3;
+        private System.Windows.Forms.PictureBox box2;
+        private System.Windows.Forms.PictureBox box1;
+        private System.Windows.Forms.PictureBox box0;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblgracz2;
+        private System.Windows.Forms.Label lblgracz1;
+        private System.Windows.Forms.PictureBox gracz2;
+        private System.Windows.Forms.PictureBox gracz1;
     }
 }
 
